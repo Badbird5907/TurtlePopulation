917969923d7abc64f4803a5d9dd92c5598d534db
Badbird-5907 <50347938+Badbird-5907@users.noreply.github.com>
2022-06-03 15:32:05 -0400

    initial commit

---
+++ src/main/java/dev/badbird/Main.java
@@ -0,0 +1,10 @@
+package dev.badbird;
+
+import java.io.File;
+
+public class Main {
+    private static final File IN_FILE = new File("data.csv");
+    public static void main(String[] args) {
+
+    }
+}
